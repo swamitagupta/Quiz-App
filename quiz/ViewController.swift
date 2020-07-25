0f8a0d074ec1c08db4ceeb8904dd8d7ba004f7ad
Swamita <swamita2001@gmail.com>
2020-07-25 10:59:30 +0530

    Initial Commit

---
+++ quiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  quiz
+//
+//  Created by Swamita on 25/07/20.
+//  Copyright © 2020 Swamita Gupta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
