fb3336669d63d1b42491b6d80d2edeaa4ad1d0f2
Swamita <swamita2001@gmail.com>
2020-08-15 10:45:36 +0530

    Basic UI Setup

--- quiz/ViewController.swift
+++ quiz/ViewController.swift
@@ -9,12 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var questionText: UILabel!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func answerPressed(_ sender: Any) {
+    }
+    
 }
 
